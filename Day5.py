cf78c24fd4a09f238b5bd9b0af4472eae62e944d
Emilio Martinez <emarti90@gmail.com>
2021-12-18 10:10:36 +0100

    Day 5: Hydrothermal Venture - Part 2

--- Day5.py
+++ Day5.py
@@ -73,3 +73,72 @@ danger = len(np.where(diag >= 2)[0])
 
 print(danger)
 
+'''
+--- Part Two ---
+Unfortunately, considering only horizontal and vertical lines doesn't give you the full picture; you need to also consider diagonal lines.
+
+Because of the limits of the hydrothermal vent mapping system, the lines in your list will only ever be horizontal, vertical, or a diagonal line at exactly 45 degrees. In other words:
+
+An entry like 1,1 -> 3,3 covers points 1,1, 2,2, and 3,3.
+An entry like 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+Considering all lines from the above example would now produce the following diagram:
+
+1.1....11.
+.111...2..
+..2.1.111.
+...1.2.2..
+.112313211
+...1.2....
+..1...1...
+.1.....1..
+1.......1.
+222111....
+You still need to determine the number of points where at least two lines overlap. In the above example, this is still anywhere in the diagram with a 2 or larger - now a total of 12 points.
+
+Consider all of the lines. At how many points do at least two lines overlap?
+'''
+
+diag = np.zeros((Xmax+1,Ymax+1),dtype=np.int)
+
+for i in range(len(vents)):
+    x1 = vents['X1'].iloc[i]
+    y1 = vents['Y1'].iloc[i]
+    x2 = vents['X2'].iloc[i]
+    y2 = vents['Y2'].iloc[i]
+
+    Xdiff = x2 - x1
+    Ydiff = y2 - y1
+
+    if Xdiff == 0: # Vertical
+        x = x1
+        if y1 > y2:
+            y2 = vents['Y1'].iloc[i]
+            y1 = vents['Y2'].iloc[i]
+        for y in np.arange(y1,y2+1):
+            diag[y][x] += 1
+
+    elif Ydiff == 0: # Horiz
+        y = y1
+        if x1 > x2:
+            x2 = vents['X1'].iloc[i]
+            x1 = vents['X2'].iloc[i]
+        for x in np.arange(x1,x2+1):
+            diag[y][x] += 1
+
+    elif (Xdiff == Ydiff) and (Xdiff > 0): # Diagonal ↗
+        for i in range(Xdiff+1):
+            diag[y1+i][x1+i] += 1
+        
+    elif (Xdiff == Ydiff) and (Xdiff < 0): # Diagonal ↙
+        for i in range(abs(Xdiff)+1):
+            diag[y1-i][x1-i] += 1
+    elif Xdiff > Ydiff: # Diagonal ↘
+        for i in range(Xdiff+1):
+            diag[y1-i][x1+i]
+    elif Xdiff < Ydiff:
+        for i in range(Ydiff+1): # Diagonal ↖
+            diag[y1+i][x1-i]
+
+danger = len(np.where(diag >= 2)[0])
+
+print(danger)
